4819a71a588ee998add281796e83dd3fb968de1b
hjrgrn <187955624+hjrgrn@users.noreply.github.com>
2024-12-13 12:44:09 +0100

    Plugins update

--- nvim/lua/user/lsp/settings/clangd.lua
+++ nvim/lua/user/lsp/settings/clangd.lua
@@ -1,4 +1,5 @@
 return {
+    -- Indentation of 4 spaces instead of 2
     cmd = {
         "clangd",
         "--fallback-style=webkit"
--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -26,9 +26,9 @@ require('lazy').setup({
 
         -- General
         -- 'nvim-lua/plenary.nvim', -- Useful lua functions used ny lots of plugins
-        { 'nvim-lua/plenary.nvim',     commit = 'a3e3bc82a3f95c5ed0d7201546d5d2c19b20d683' }, -- Useful lua functions used by lots of plugins
+        { 'nvim-lua/plenary.nvim',     commit = '2d9b06177a975543726ce5c73fca176cedbffe9d' }, -- Useful lua functions used by lots of plugins
         -- 'nvim-lualine/lualine.nvim',
-        { 'nvim-lualine/lualine.nvim', commit = '544dd1583f9bb27b393f598475c89809c4d5e86b' },
+        { 'nvim-lualine/lualine.nvim', commit = '2a5bae925481f999263d6f5ed8361baef8df4f83' },
         'norcalli/nvim-colorizer.lua',
         'tpope/vim-commentary',
         -- 'lewis6991/impatient.nvim', -- this may be necessary for not having to deal with lazy loading
@@ -36,27 +36,27 @@ require('lazy').setup({
         {
             'folke/todo-comments.nvim',
             event = 'VimEnter',
-            commit = '8f45f353dc3649cb9b44cecda96827ea88128584',
+            commit = 'ae0a2afb47cf7395dc400e5dc4e05274bf4fb9e0',
             dependencies = { 'nvim-lua/plenary.nvim' },
             -- opts = { signs = false }
         },
 
         -- UI
         -- 'kyazdani42/nvim-web-devicons',
-        { 'kyazdani42/nvim-web-devicons', commit = '3722e3d1fb5fe1896a104eb489e8f8651260b520' },
+        { 'kyazdani42/nvim-web-devicons', commit = 'e73d2774d12d0ecf9e05578d692ba1ea50508cf2' },
         -- 'kyazdani42/nvim-tree.lua',
-        { 'kyazdani42/nvim-tree.lua',     commit = '48d0e82f9434691cc50d970898142a8c084a49d6' },
+        { 'kyazdani42/nvim-tree.lua',     commit = 'db8d7ac1f524fc6f808764b29fa695c51e014aa6' },
         -- 'goolord/alpha-nvim',
         -- 'mhinz/vim-startify',
         { 'mhinz/vim-startify',           commit = '4e089dffdad46f3f5593f34362d530e8fe823dcf' },
         -- 'akinsho/bufferline.nvim',
-        { 'akinsho/bufferline.nvim',      commit = '0b2fd861eee7595015b6561dade52fb060be10c4' },
+        { 'akinsho/bufferline.nvim',      commit = '261a72b90d6db4ed8014f7bda976bcdc9dd7ce76' },
 
         -- Colorschemes
         -- 'folke/tokyonight.nvim',
         {
             'folke/tokyonight.nvim',
-            commit = 'b0e7c7382a7e8f6456f2a95655983993ffda745e',
+            commit = 'b262293ef481b0d1f7a14c708ea7ca649672e200',
             lazy = false,
             priority = 1000,
             opts = {},
@@ -67,15 +67,15 @@ require('lazy').setup({
 
         -- cmp plugins
         -- 'hrsh7th/nvim-cmp',
-        { 'hrsh7th/nvim-cmp',         commit = 'ae644feb7b67bf1ce4260c231d1d4300b19c6f30' }, -- The completion plugin
+        { 'hrsh7th/nvim-cmp',         commit = '3403e2e9391ed0a28c3afddd8612701b647c8e26' }, -- The completion plugin
         -- 'hrsh7th/cmp-buffer', -- buffer completions
         { 'hrsh7th/cmp-buffer',       commit = '3022dbc9166796b644a841a02de8dd1cc1d311fa' }, -- buffer completions
         -- 'hrsh7th/cmp-path', -- path completions
         { 'hrsh7th/cmp-path',         commit = '91ff86cd9c29299a64f968ebb45846c485725f23' }, -- path completions
         -- 'saadparwaiz1/cmp_luasnip', -- snippet completions
-        { 'saadparwaiz1/cmp_luasnip', commit = '05a9ab28b53f71d1aece421ef32fee2cb857a843' }, -- snippet completions
+        { 'saadparwaiz1/cmp_luasnip', commit = '98d9cb5c2c38532bd9bdb481067b20fea8f32e90' }, -- snippet completions
         -- 'hrsh7th/cmp-nvim-lsp',
-        { 'hrsh7th/cmp-nvim-lsp',     commit = '39e2eda76828d88b773cc27a3f61d2ad782c922d' },
+        { 'hrsh7th/cmp-nvim-lsp',     commit = '99290b3ec1322070bcfb9e846450a46f6efa50f0' },
         -- 'hrsh7th/cmp-nvim-lua',
         { 'hrsh7th/cmp-nvim-lua',     commit = 'f12408bdb54c39c23e67cab726264c10db33ada8' },
 
@@ -93,7 +93,7 @@ require('lazy').setup({
 
         -- LSP
         -- 'neovim/nvim-lspconfig', -- enable LSP
-        { 'neovim/nvim-lspconfig',             commit = '64073cbed0ce23e988160bfd1a148a75b6af94cc' },                -- enable LSP
+        { 'neovim/nvim-lspconfig',             commit = '124be12d782d656b3c75b513a44d9e4728406078' },                -- enable LSP
         -- {'williamboman/mason.nvim', config = true},
         { 'williamboman/mason.nvim',           commit = 'e2f7f9044ec30067bc11800a9e266664b88cda22', config = true }, -- simple to use language server installer
         -- 'williamboman/mason-lspconfig.nvim',
@@ -108,7 +108,7 @@ require('lazy').setup({
         -- TreeSitter
         { -- Highlight, edit, and navigate code
             'nvim-treesitter/nvim-treesitter',
-            commit = 'c79b068f395817c39ed147d4ee76855c36b846e6',
+            commit = '33d27173876499a0f037cb9edff90d583711b6ae',
             build = ":TSUpdate",
         },
 
