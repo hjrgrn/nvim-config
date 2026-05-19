c7007d87e80c06611637253164b8780de77d3f81
hjrgrn <187955624+hjrgrn@users.noreply.github.com>
2026-05-19 20:29:08 +0200

    lsp: Add Hyprland stubs to lua_ls configuration

--- nvim/lua/user/lsp/settings/lua_ls.lua
+++ nvim/lua/user/lsp/settings/lua_ls.lua
@@ -1,19 +1,33 @@
+local hyprland_path = '/usr/share/hypr/stubs'
+local hyprland = vim.fn.isdirectory(hyprland_path) == 1
+local globals = { 'vim' }
+local library = {
+    -- Tells lua_ls where to find all the Lua files that you have loaded
+    -- for your neovim configuration.
+    '${3rd}/luv/library',
+    unpack(vim.api.nvim_get_runtime_file('', true)),
+}
+
+-- If Hyprland is installed, add
+if hyprland then
+    table.insert(globals, 'hl')
+    table.insert(library, hyprland_path)
+end
+
 return {
     settings = {
         Lua = {
             runtime = { version = 'LuaJIT' },
             workspace = {
                 checkThirdParty = false,
-                -- Tells lua_ls where to find all the Lua files that you have loaded
-                -- for your neovim configuration.
-                library = {
-                    '${3rd}/luv/library',
-                    unpack(vim.api.nvim_get_runtime_file('', true)),
-                },
+                library = library,
             },
             telemetry = { enable = false },
             completion = {
                 callSnippet = 'Replace',
+            },
+            diagnostics = {
+                globals = globals,
             },
         },
     },
